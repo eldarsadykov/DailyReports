3f6208583c1115f4af88084bd7da984b9302a9aa
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2025-09-25 07:54:54 +0200

    Update models

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    key = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=255)
+    progress = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+    )
+
+    class Meta:
+        unique_together = ('key', 'name')
+        
+    def __str__(self):
+        return f"{self.key } {self.name} - {self.progress}%"
