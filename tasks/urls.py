dcd721e7b99665e51e95c9a4c0a828e97fe6bb3f
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2025-09-26 21:49:38 +0200

    feat: Add task view

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_tasks, name='search'),
+    path('create/', views.create_task, name='create-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.db.models import Q
+from django.views.decorators.http import require_http_methods
 
 from tasks.forms import TaskForm
 
@@ -9,7 +10,6 @@ from tasks.forms import TaskForm
 def index(request):
     tasks = request.user.tasks.all().order_by('-created_at')
     form = TaskForm()
-    print(form)
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks.html', context)
 
@@ -24,3 +24,17 @@ def search_tasks(request):
     )
     context = {'tasks': tasks}
     return render(request, 'partials/task-list.html', context)
+
+
+@login_required
+@require_http_methods(['POST'])
+def create_task(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.user = request.user
+        task.save()
+        context = {'task': task}
+        response = render(request, 'partials/task-row.html', context)
+        response['HX-Trigger'] = 'create-task-success'
+        return response
