7031e6c878654e56c0682aad5b2bec29b8df566f
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2025-09-28 17:47:11 +0200

    fix(build): Run npm before collecting static files.

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+npm install
+npm run build
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
@@ -8,6 +11,3 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py createsuperuser --no-input || true
-
-npm install
-npm run build
